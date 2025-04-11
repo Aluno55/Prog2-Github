872e0757f0cba657cdb13cd723abef4f69921383
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-04-11 11:42:04 -0300

    Not sure how to read
    not sure if i should use fscanf, fseek, feof or something else

--- Projeto01.c
+++ Projeto01.c
@@ -4,26 +4,27 @@
 
 int aquilo(){
 int repeats = 1;
-char title[1030];
-char searchword = ' ' [50];
+char title[1000];
+char searchword[50] = ' ' ;
 
     printf("Insira o nome do arquivo: ");
-    scanf("%s", title);
+    scanf("%s.txt", title);
 
     FILE *f = fopen(title, "r");
 
     if (!f) {printf("Erro na abertura do arquivo\n");return 1;}
-
-    printf("Insira uma palavra para procurar (nada para quantidade maxima): ");
-    scanf("%s", searchword);
+    scanf("%[^\n]", searchword);
 
     while (!feof(f)) {
     if(){
+        
+        fscanf(f, "%s", );
         fseek(f, sizeof (title), SEEK_SET);
     }
-
-
-
+    repeats++
     }
 
+    
+
+    
     fclose(f);return 0;}
