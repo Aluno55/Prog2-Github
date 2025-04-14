014fc2c729dd1849d1c82ffe74666a75ab0a72b1
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-04-14 15:35:16 -0300

    not sure
    not sure if this works, reading word per word

--- Projeto01.c
+++ Projeto01.c
@@ -3,9 +3,10 @@
 #include "Projeto01.h"
 
 int aquilo(){
+char space = ' ';
 int repeats = 1;
 char title[1000];
-char searchword[50] = ' ' ;
+char searchword[1000];
 
     printf("Insira o nome do arquivo: ");
     scanf("%s.txt", title);
@@ -15,13 +16,10 @@ char searchword[50] = ' ' ;
     if (!f) {printf("Erro na abertura do arquivo\n");return 1;}
     scanf("%[^\n]", searchword);
 
-    while (!feof(f)) {
-    if(){
-        
-        fscanf(f, "%s", );
-        fseek(f, sizeof (title), SEEK_SET);
-    }
-    repeats++
+        while (fscanf(f, " %s", title) == 1) {
+        puts(title);
+        if (title == searchword)
+        repeats++;
     }
 
     
