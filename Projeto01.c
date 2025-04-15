71996ced4688af18d8f784e96985f3b97294e1ec
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-04-15 17:46:31 -0300

    thanks gods it works

--- Projeto01.c
+++ Projeto01.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include "Projeto01.h"
 
-int counterWord(){
-    char space = ' ';
-    int repeats = 1;
+int counterWord() {
+    int repeats = 0;
     char title[1000];
     char searchword[1000];
 
     printf("Insira o nome do arquivo: ");
-    scanf("%s %[^\n]", title, searchword);
+    scanf("%s", title);
 
     FILE *f = fopen(title, "r");
 
-    if (!f) {printf("Erro na abertura do arquivo\n");return 1;}
+    if (!f) {
+        printf("Erro na abertura do arquivo\n");
+        return 1;
+    }
 
     while (fscanf(f, " %s", title) == 1) {
         puts(title);
-        if (searchword[0] == space) {
-            repeats++;
-    };
+        repeats++;
     }
-
-    fclose(f);return 0;}
+    printf("Palavras:%d\n", repeats);
+    fclose(f);
+    return 0;
+}
