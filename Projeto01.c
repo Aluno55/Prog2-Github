2fa8d218673048d143edbe16f825845af991f0bb
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-05-06 17:37:31 -0300

    nsei

--- Projeto01.c
+++ Projeto01.c
@@ -21,6 +21,9 @@ int counterWord(int count, char *search[]) {
     char vine[500];
     char limbo[]= " \n.,;:!?'()[]{}<>-_/|@#$%^&*+=";
 
+
+
+
     printf("Insira o nome do arquivo: ");
     scanf("%s", title);
     // não entendi a busca de palavra
@@ -28,19 +31,17 @@ int counterWord(int count, char *search[]) {
     FILE *f = fopen(title, "r");
     if (!f) {printf("Erro na abertura do arquivo\n");return 1;}
 
-    while (!feof(f) && total < 1000) {
+    while (!feof(f) && total < 1000 && fscanf(f, " %s", vine) != 0) {
         fgets(vine, sizeof(vine), f);
         char* token = strtok(vine, limbo);
         while (token != NULL) {total++;
             minus(token);
             if (strcmp(token, wordle) == 0) {specific++;}
             token = strtok(NULL, limbo);}
-    }
-    //não tenho certeza dessa parte acima, não testei ainda
-    //deve pega a linha do arquivo e colocar palavra por palavra no token e comparar a searchword com o token ambos em minusculo
+     }
 
     if(specific>0){printf("Quantidade da Palavra Selecionada: %d\n", specific);}
-    else{printf("Palavras Totais (Max 1000): %d\n", count-1);}
+    else{printf("Palavras Totais (Max 1000): %d\n", total);}
 
     printf("Tempo de Execução da CPU: %.5ld\n", (clock() - cpu) / CLOCKS_PER_SEC);
     printf("Tempo de Execução no fisico: %.5ld segundos\n", time(NULL) - irl);
--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "Projeto01.h"
 
 // argc conta os argumentos
 // *argv[X] pega o argumento em X
 int main(int argc, char *argv[]){
+
+    if (argc == 1) {printf("Falta informar");} else if (argc == 2){
+    totalWords} else if (argc == 3){searchWords} else if (argc == 4){strcmp(token, wordle)}
     counterWord(argc, argv);
    return 0;}
