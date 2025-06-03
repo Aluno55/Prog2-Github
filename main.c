1707aad0b245e4bf2931857ac04cc4a90c3031bb
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-03 16:34:37 -0300

    falha fatal

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 #include "Projeto01.h"
 
 int main() {
-    printf("%d\n", potency(5,2));
-    printf("%d\n", fatorial(3));
-    printf("%d\n", recursiva(3,3));
-    printf("%d\n", gold(3,3));
+    printf("%d\n", gold(3));
 return 0;
 }
