74daf7049d6a7f7e10939cb632df0300de63a6d5
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-03 16:39:07 -0300

    falha fatal

--- Projeto01.c
+++ Projeto01.c
@@ -26,5 +26,5 @@ int fatorial(int fat) {
 int gold (int ag) {
     if (ag == 0) return 0;
     if (ag == 1) return 1;
-    if (ag <= 2) return gold(ag-1) + gold(ag-2);
+    return gold(ag-1) + gold(ag-2);
 }
