d6cf17b8b0f103fe9f5351ad361aec17b4e390bf
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-03 15:51:47 -0300

    coiso

---
+++ projeto02.c
@@ -0,0 +1,5 @@
+//
+// Created by aluno on 03/06/25.
+//
+
+#include "projeto02.h"
---
+++ projeto02.h
@@ -0,0 +1,8 @@
+//
+// Created by aluno on 03/06/25.
+//
+
+#ifndef PROJETO02_H
+#define PROJETO02_H
+
+#endif //PROJETO02_H
