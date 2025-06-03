4e59a7c4bcc1e51e1af134d858dc95526cb9b823
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-03 16:38:08 -0300

    falha fatal

--- projeto02.c
+++
@@ -1,9 +0,0 @@
-#include "projeto02.h"
-
-
-// pilha actions, char [solicitar, decricao, prioridade, acoes]
-// fila, char [chamado, tamanho, capacity]
-
-
-//historico usa lista cadeada
-//atendimento ação na pilha
--- projeto02.h
+++
@@ -1,4 +0,0 @@
-#ifndef PROJETO02_H
-#define PROJETO02_H
-
-#endif //PROJETO02_H
