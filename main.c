56a3e19b0a036bacd5c348fd3e8b6e7d11f18856
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-10 16:46:17 -0300

    itsa

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <time.h>
-
 #include "recursive.h"
 
 int main() {
     clock_t start = clock(), end;
 
-
-
-
-
-
-
-
     end = clock();
     printf("\nTime: %f", (double) (end-start)/CLOCKS_PER_SEC);
-//O(2^n)
 return 0;
 }
