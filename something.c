f41255e889f361cf7058c33f5bec02bf87a4b689
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2025-06-13 16:51:13 -0300

    idk

--- something.c
+++ something.c
@@ -1,1 +1,6 @@
 #include "something.h"
+
+// vetor de 10 elementos: 32, 12, 45, 23, 55, 98, 53, 11, 3, 65
+// cria um vetor de mesmo tamanho
+// compare X com vetor[i], aumentara com for ++i.
+// quando X for menor que todos os vetor[i], adiciona no segundo vetor
